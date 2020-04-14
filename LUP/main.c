fa5222ebbb5a6d15e0aaffc0a36d4533edd5870d
Artem Belov <inctnce@gmail.com>
2020-04-14 15:26:57 +0600

    Initial Commit

---
+++ LUP/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  LUP
+//
+//  Created by Артем on 14.04.2020.
+//  Copyright © 2020 Артем. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
